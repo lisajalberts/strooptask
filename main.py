438fb07c7372ead231b52e2fa0c2c2b3f6b5e46b
lisajalberts <l.nieuwenhuize@student.maastrichtuniversity.nl>
2025-02-27 19:15:44 +0100

    adds .gitignore file

--- main.py
+++ main.py
@@ -22,10 +22,13 @@ random_colour = random.choice(colors)
 
 trials = [1, 2, 3,4,5]
 # loop for more random trials
+
 for rounds in trials:
  random_colour = random.choice(colors)
  random_colourtext = random.choice(list_colours)
+
  print(f"{random_colour}{random_colourtext}\033[0m")# Randomly pick a color and print the text
+
  input_user = input("Which colour was the text? ").lower()# Get user input
  if input_user == color_dict[random_colour]:# Check if input matches the displayed color
      print("Correct!")
